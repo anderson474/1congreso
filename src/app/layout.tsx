95011d9ed3f0914e1a49912268ee8975867c69aa
Jhon Anderson Galeano Santillana <andersongs.474@gmail.com>
2025-07-18 12:15:34 -0500

    cambio de la imagend e perfil en la metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,28 +5,28 @@ import { Nunito, Lora, Inter } from "next/font/google";
 import Script from "next/script";
 
 const nunito = Nunito({
-  subsets: ['latin'],
-  weight: ['400', '700'],
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 // Configura las fuentes
-const inter = Inter({ 
-  subsets: ['latin'],
-  variable: '--font-inter', // Variable para la fuente principal
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter", // Variable para la fuente principal
 });
 
 const lora = Lora({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-  variable: '--font-lora', // Variable para la fuente de la cita
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  variable: "--font-lora", // Variable para la fuente de la cita
 });
 
 export const metadata: Metadata = {
   title: "Congreso Internacional",
   description: "El congreso",
   icons: {
-    icon: "/RevistaCientifica.png", // Asegúrate de tenerlo en /public
+    icon: "/perfil.png", // Asegúrate de tenerlo en /public
   },
 };
 
@@ -37,11 +37,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className={`${nunito.className} ${inter.variable} ${lora.variable}`}>
+      <body
+        className={`${nunito.className} ${inter.variable} ${lora.variable}`}
+      >
         {children}
       </body>
     </html>
   );
 }
-
-
