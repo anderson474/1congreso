b8636a0b886cef8ed9ff0469229fb139f15590c3
Jhon Anderson Galeano Santillana <andersongs.474@gmail.com>
2025-07-23 11:06:06 -0500

    adicion de meta ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito, Lora, Inter } from "next/font/google";
-import Script from "next/script";
+import Head from "@node_modules/next/head";
+import dynamic from "next/dynamic";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -54,6 +55,9 @@ export const metadata: Metadata = {
     ], // URL ABSOLUTA
   },
 };
+const PixelTracker = dynamic(() => import("@/components/pixelTracker"), {
+  ssr: false,
+});
 
 export default function RootLayout({
   children,
@@ -62,9 +66,40 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?                         
+              n.callMethod.apply(n,arguments):n.queue.push   
+              (arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!
+              0;n.version='2.0';n.queue=[];t=b.createElement(e);
+              t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,
+              'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '678309121876043');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=
+            PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
       <body
         className={`${nunito.className} ${inter.variable} ${lora.variable}`}
       >
+        <PixelTracker />
         {children}
       </body>
     </html>
